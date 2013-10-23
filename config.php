5468b6894c876c8df08d8f7cfd2e0268a181f5f5
ozz <ozzozz@gmail.com>
2013-10-23 20:54:45 +0900

    change config

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php 
 
-/*
 // Override any of the default settings below:
 
 $config['site_title'] = 'Pico';			// Site title
-$config['base_url'] = ''; 				// Override base URL (e.g. http://example.com)
-$config['theme'] = 'default'; 			// Set the theme (defaults to "default")
-$config['date_format'] = 'jS M Y';		// Set the PHP date format
+$config['base_url'] = 'http://labo.ozz.in/Pico'; 				// Override base URL (e.g. http://example.com)
+$config['theme'] = 'ozz'; 			// Set the theme (defaults to "default")
+$config['date_format'] = 'Y-m-d';		// Set the PHP date format
 $config['twig_config'] = array(			// Twig settings
 	'cache' => false,					// To enable Twig caching change this to CACHE_DIR
 	'autoescape' => false,				// Autoescape Twig vars
@@ -19,5 +18,3 @@ $config['excerpt_length'] = 50;			// The pages excerpt length (in words)
 // To add a custom config setting:
 
 $config['custom_setting'] = 'Hello'; 	// Can be accessed by {{ config.custom_setting }} in a theme
-
-*/
